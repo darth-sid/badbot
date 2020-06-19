636fdb45c906581bed670f499ad947abe4810401
darth-sid <55854690+darth-sid@users.noreply.github.com>
2020-06-19 16:15:01 -0700

    e

--- fubot.py
+++ fubot.py
@@ -1,1 +1,24 @@
-cd .
+import os,random
+import discord
+from dotenv import load_doten
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author ==  client.user:
+        return
+    if message.content == 'censor':
+        censored = True
+    if message.content == 'f':
+        if censored:
+            await message.channel.send('||fuck you||')
+        else:
+            await message.channel.send('fuck you')
