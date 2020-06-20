edf955cca8ad333f964245da79a408c2e07181cf
darth-sid <55854690+darth-sid@users.noreply.github.com>
2020-06-20 12:34:01 -0700

    next

--- fubot.py
+++ fubot.py
@@ -3,10 +3,9 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('Fbot')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
